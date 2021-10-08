5a8b7af514d273ec98b21b10cae02559779bf937
Marven9999 <91346861+Marven9999@users.noreply.github.com>
2021-10-08 16:37:15 +0200

    OrbitingProjectile Stuff

---
+++ TrinitarianPlayer.cs
@@ -0,0 +1,187 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.DataStructures;
+using System;
+
+namespace Trinitarian
+{
+	public class TrinitarianPlayer : ModPlayer
+	{
+		public int TitleID;
+		public bool FocusBoss;
+	    public bool ShowText;
+
+		public int ScreenShake;
+        public bool canFocus = true;
+        //These are all important for the Orbiting staff. They could be used for other uses though. The orbiting staff uses at most the first 15 of the OrbitingProjectile array so the rest are free to use for other weapons.
+        public int RotationTimer = 0;
+        public int[] OrbitingProjectileCount = new int[5];                               //Current upadted count of how many projectiles are active.
+        public Vector2[,] OrbitingProjectilePositions = new Vector2[5, 50];             //Used to store the desired positions for the projectiles.
+        public Projectile[,] OrbitingProjectile = new Projectile[5, 50];                //This stores all the projectiles that are currently beeing used. A projectiles ID is equal to the index in this array.
+        //End of orbiting projectile stuff.
+
+        public Vector2[] PreviousVelocity = new Vector2[30];
+        // private float amount = 0;
+
+        //fuck me
+        /* Related to non-functional boss code below
+         * Vector2 screenPositionStore;
+        private Vector2 focusTo;
+        private float towardsLength;
+        private bool holdPosition;
+        private int holdCounter = 0;
+        private int holdCameraLength;
+        private float returnLength; */
+
+        public bool drowning = false;
+        public override void OnEnterWorld(Player player)
+        {
+            for (int i = 0; i < OrbitingProjectileCount.Length; i++)
+            {
+                OrbitingProjectileCount[i] = 0;
+            }
+        }
+        public override void ResetEffects()
+        {
+            drowning = false;
+        }
+        public override void UpdateDead()
+        {
+            drowning = false;
+            for (int i = 0; i < OrbitingProjectileCount.Length; i++)
+            {
+                OrbitingProjectileCount[i] = 0;
+            }
+        }
+        //This is where we make our central timer that the orbiting projectile uses.
+        public override void PostUpdate()
+        {
+            bool temp = false;
+            for (int i = 0; i < 5; i++)
+            {
+                if (OrbitingProjectileCount[i] > 0) temp = true;
+            }
+            if (temp)
+            {
+                //The resetting the timer every 300 is not necessary but makes the period of the timer more apperant. 
+                GenerateProjectilePositions();
+                RotationTimer++;
+            }
+            else RotationTimer = 0;
+        }
+        public override void UpdateLifeRegen()
+        {
+            if (drowning)
+            {
+                if (player.lifeRegen > 0)
+                {
+                    player.lifeRegen = 0;
+                }
+                player.lifeRegenTime = 0;
+                player.lifeRegen -= 8; //change this number to how fast you want the debuff to damage the players. Every 2 is 1 hp lost per second
+            }
+        }
+
+         public override bool PreKill(double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource)
+         {
+            if (drowning)
+            {
+                int messageType = Main.rand.Next(4); //the number of different types of death messages you want to have
+                if (messageType == 0 && damage == 10.0 && hitDirection == 0 && damageSource.SourceOtherIndex == 8) //messagetype == 0
+                {
+                    damageSource = PlayerDeathReason.ByCustomReason(player.name + " forgot to breathe.");
+                }
+                else if (messageType == 1 && damage == 10.0 && hitDirection == 0 && damageSource.SourceOtherIndex == 8) //messagetype == 1
+                {
+                    damageSource = PlayerDeathReason.ByCustomReason(player.name + " is sleeping with the fish.");
+                }
+                else if (messageType == 2 && damage == 10.0 && hitDirection == 0 && damageSource.SourceOtherIndex == 8) //messagetype == 2 etc
+                {
+                    damageSource = PlayerDeathReason.ByCustomReason(player.name + " drowned.");
+                }
+                else if (messageType == 3 && damage == 10.0 && hitDirection == 0 && damageSource.SourceOtherIndex == 8)
+                {
+                    damageSource = PlayerDeathReason.ByCustomReason(player.name + " is shark food.");
+                }
+            }
+            return base.PreKill(damage, hitDirection, pvp, ref playSound, ref genGore, ref damageSource);
+         }
+
+		public override void ModifyScreenPosition()
+        {
+            /* if (FocusBoss)
+            {
+                if (canFocus)
+                {
+                    if (!Main.gamePaused)
+                    {
+                        screenPositionStore = new Vector2(MathHelper.Lerp(player.Center.X - Main.screenWidth / 2, focusTo.X - Main.screenWidth / 2, amount), MathHelper.Lerp(player.Center.Y - Main.screenHeight / 2, focusTo.Y - Main.screenHeight / 2, amount));
+                    }
+
+                    Main.screenPosition = screenPositionStore;
+                    amount += 1 / towardsLength;
+                    if (amount >= 1f)
+                    {
+                        holdPosition = true;
+                        canFocus = false;
+                        amount = 0;
+                    }
+                }
+                else
+                {
+                    if (holdPosition)
+                    {
+                        Main.screenPosition = screenPositionStore;
+                        holdCounter++;
+
+                        if (holdCounter == holdCameraLength)
+                        {
+                            holdCounter = 0;
+                            holdPosition = false;
+                        }
+                    }
+                    else
+                    {
+                        if (!Main.gamePaused)
+                        {
+                            screenPositionStore = new Vector2(MathHelper.SmoothStep(focusTo.X - Main.screenWidth / 2, player.Center.X - Main.screenWidth / 2, amount), MathHelper.SmoothStep(focusTo.Y - Main.screenHeight / 2, player.Center.Y - Main.screenHeight / 2, amount));
+                        }
+                        Main.screenPosition = screenPositionStore;
+
+                        amount += 1 / returnLength;
+
+                        if (amount >= 1f)
+                        {
+                            amount = 0;
+                            FocusBoss = false;
+                            canFocus = true;
+                            ShowText = false;
+                        }
+                    }
+                }
+            } */
+
+            if (!Main.gamePaused)
+            {
+                if (ScreenShake > 0)
+                {
+                    Main.screenPosition += new Vector2(Main.rand.Next(-10, 10), Main.rand.Next(-10, 10));
+                    ScreenShake--;
+                }
+            }
+
+
+        }
+        //oudated. Will delete soon.
+        public void GenerateProjectilePositions()
+        {           
+            double period = 2f * Math.PI / 300f;
+            for (int i = 0; i < OrbitingProjectileCount[0]; i++)
+            {
+                //Radius 200.
+                OrbitingProjectilePositions[0, i] = player.Center + new Vector2(200 * (float)Math.Cos(period * (RotationTimer + (300 / OrbitingProjectileCount[0] * i))), 200 * (float)Math.Sin(period * (RotationTimer + (300 / OrbitingProjectileCount[0] * i))));
+            }
+        }
+    }
+}
